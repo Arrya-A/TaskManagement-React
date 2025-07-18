2ae8fe0694e4ed53e25e7d81d1c28e60f2eb851e
arrya <arrya@bpract.com>
2025-07-18 15:28:12 +0530

    v

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import './App.css'
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Landing from "./pages/Landing";
+import Login from "./Auth/Login";
+import Register from "./Auth/Register";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
-
   return (
     <>
-
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/home" element={<Dashboard />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
